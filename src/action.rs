a9d3380d3170f277d8930818e6893b4cdf6cf23d
Em Vance <ayv200@gmail.com>
2025-09-03 20:04:14 +0200

    more 'new' action  corrections

--- src/action.rs
+++ src/action.rs
@@ -196,7 +196,7 @@ pub fn init(library: bool, is_c: bool, clangd: bool) -> Result<(), Error> {
         } else {
             "#pragma once\n\nint func(int a, int b);\n"
         })?;
-        let toml = format!("[build]\npackage = \"{name}\"\nversion = \"0.1.0\"\nlang = \"{lang}\"\nkind = \"staticlib\"\ninclude = [ \"src\", \"include/{name}\" ]\ninclude-pub = \"include\"\n\n[dependencies]\n");
+        let toml = format!("[build]\npackage = \"{name}\"\nversion = \"0.1.0\"\nlang = \"{lang}\"\nkind = \"staticlib\"\ninclude = [ \"src\", \"include/{name}\" ]\ninclude-pub = \"include\"\n\ndependencies = []\n");
         std::fs::write(format!("src/lib.{ext}"), "#include \"lib.h\"\n\nint func(int a, int b) {\n    return a + b;\n}\n")?;
         std::fs::write("Vango.toml", &toml)?;
         if clangd {
@@ -204,7 +204,7 @@ pub fn init(library: bool, is_c: bool, clangd: bool) -> Result<(), Error> {
             generate(build.unwrap_build())?;
         }
     } else {
-        let toml = format!("[build]\npackage = \"{name}\"\nversion = \"0.1.0\"\nlang = \"{lang}\"\n\n[dependencies]\n");
+        let toml = format!("[build]\npackage = \"{name}\"\nversion = \"0.1.0\"\nlang = \"{lang}\"\n\ndependencies = []\n");
         std::fs::write(format!("src/main.{ext}"), format!("#include <{header}>\n\n\nint main() {{\n    printf(\"Hello World!\\n\");\n}}\n"))?;
         std::fs::write("Vango.toml", &toml)?;
         if clangd {

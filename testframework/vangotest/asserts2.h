bee5ab861c67a8cfce3c3206579fbf051a8ed4e5
Em Vance <ayv200@gmail.com>
2025-09-09 19:27:45 +0200

    forgot to finish the C++ version...

--- testframework/vangotest/asserts2.h
+++ testframework/vangotest/asserts2.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstdint>
 #include <iostream>
 #include <sstream>
 #include <exception>
@@ -21,7 +22,12 @@ public:
     }
 };
 
-typedef void(*TestFunc)();
+typedef void(*TestFuncImpl)();
+
+struct TestFunc {
+    const char* id;
+    TestFuncImpl fn;
+};
 
 }
 
@@ -62,42 +68,75 @@ typedef void(*TestFunc)();
 #error Unsupported compiler
 #endif
 
-#define vango_test(name) void name(); __declspec(allocate("vgtest$v")) ::vango::TestFunc _vango_test_##name = name; void name()
+#define vango_test(name) \
+    void name(); \
+    VANGO_SECTION_TESTS ::vango::TestFunc _vango_test_##name = { #name, &name }; \
+    void name()
 
 
 #ifdef VANGO_TEST_ROOT
 
-#define run_test(k, f) try { \
-        (f)(); \
-        std::cerr << "\033[32m[VanGo:  info] passed: '" << k << "'\033[m\n"; \
-    } catch (const ::vango::AssertionFail& e) { \
-        std::cerr << "\033[32m[VanGo:  info] \033[31mfailed: '" << k << "' on line " << e.failline << ": \033[m" << e.msg << "\n"; \
-    }
-
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
 #if defined(_MSC_VER)
 
-__declspec(allocate("vgtest$a")) uint8_t _start_vango_tests = 0;
-__declspec(allocate("vgtest$z")) uint8_t _stop_vango_tests = 0;
+__declspec(allocate("vgtest$a")) ::vango::TestFunc _start_vgtest = {};
+__declspec(allocate("vgtest$z")) ::vango::TestFunc _stop_vgtest = {};
 
 int main(int argc, char** argv) {
-    ::vango::TestFunc* _vg_start = (vango::TestFunc*)(&_start_vango_tests);
-    ::vango::TestFunc* _vg_stop = (vango::TestFunc*)(&_stop_vango_tests);
+    char** _vg_begin = (char**)(&_start_vgtest+1);
+    char** _vg_end = (char**)&_stop_vgtest;
 
-    while (_vg_start++ < _vg_stop) {
-        if (*_vg_start == 0) {  continue; }
-        run_test("abc", *_vg_start);
-        _vg_start++;
+    for (_vg_begin; _vg_begin < _vg_end; _vg_begin++) {
+        if (*_vg_begin == 0) { continue; }
+        vango::TestFunc* _vg_f = (vango::TestFunc*)_vg_begin;
+        int _vg_run_this = argc == 1 ? 1 : 0;
+        if (argc > 1) {
+            for (int i = 1; i < argc; i++) {
+                if (strcmp(_vg_f->id, argv[i]) == 0) {
+                    _vg_run_this = 1;
+                    break;
+                }
+            }
+        }
+        if (_vg_run_this != 0) {
+            try {
+                (_vg_f->fn)();
+                std::cerr << "\033[32m[VanGo:  info] passed: '" << _vg_f->id << "'\033[m\n";
+            } catch (const ::vango::AssertionFail& e) {
+                std::cerr << "\033[32m[VanGo:  info] \033[31mfailed: '" << _vg_f->id << "' on line " << e.failline << ": \033[m" << e.msg << "\n";
+            }
+            _vg_begin++;
+        }
     }
 }
 
 #elif defined(__clang__) || defined(__GNUC__)
 
-extern vango::TestFunc __start_vgtest[];
-extern vango::TestFunc __stop_vgtest[];
+extern ::vango::TestFunc __start_vgtest[];
+extern ::vango::TestFunc __stop_vgtest[];
+
 int main(int argc, char** argv) {
-    for (vango::TestFunc* t = __start_vango_tests; t != __stop_vango_tests; t++) {
-        (*t)();
+    for (::vango::TestFunc* _vg_f = __start_vgtest; _vg_f != __stop_vgtest; ++_vg_f) {
+        int _vg_run_this = argc == 1 ? 1 : 0;
+        if (argc > 1) {
+            for (int i = 1; i < argc; i++) {
+                if (strcmp(_vg_f->id, argv[i]) == 0) {
+                    _vg_run_this = 1;
+                    break;
+                }
+            }
+        }
+        if (_vg_run_this != 0) {
+            try {
+                (_vg_f->fn)();
+                std::cerr << "\033[32m[VanGo:  info] passed: '" << _vg_f->id << "'\033[m\n";
+            } catch (const ::vango::AssertionFail& e) {
+                std::cerr << "\033[32m[VanGo:  info] \033[31mfailed: '" << _vg_f->id << "' on line " << e.failline << ": \033[m" << e.msg << "\n";
+            }
+        }
     }
 }
 
